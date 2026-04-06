acb17f8d6c8586880071908e7d1de7f38d373783
ClckZ <jannisurb@gmail.com>
2026-04-06 16:04:33 +0200

    Initial commit

---
+++ QRGen/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace QRGen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
